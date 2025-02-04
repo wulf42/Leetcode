4ed5940caf3f2730c8d5edc87f56ce695f08c21d
Wojciech Pakoca <59651907+wulf42@users.noreply.github.com>
2025-02-04 23:35:51 +0100

    Time: 738 ms (49.79%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,19 @@
+-- Write your PostgreSQL query statement below
+WITH FirstOrders AS (
+  SELECT 
+    customer_id,
+    min(order_date) AS order_date,
+    min(customer_pref_delivery_date) AS customer_pref_delivery_date
+  FROM 
+  Delivery
+  GROUP BY 
+  customer_id
+)
+SELECT 
+ROUND(
+COUNT ( order_date ) FILTER (WHERE order_date=customer_pref_delivery_date ) *100.0
+/ COUNT (*) 
+,2)
+as immediate_percentage 
+FROM
+FirstOrders;
