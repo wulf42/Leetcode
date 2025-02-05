bc5dc97685c88799eeeda500d247c17c0b4b1f8b
Wojciech Pakoca <59651907+wulf42@users.noreply.github.com>
2025-02-06 00:14:20 +0100

    Time: 1819 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,7 +1,7 @@
 -- Write your PostgreSQL query statement below
 SELECT 
     activity_date AS day,
-    COUNT(DISTINCT user_id)AS active_users 
+    COUNT(DISTINCT user_id) AS active_users 
 FROM 
     Activity
 WHERE
