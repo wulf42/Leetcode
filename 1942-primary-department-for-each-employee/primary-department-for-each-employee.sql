04a3955e04e93bcead2e8e83cebb1cc689728ca3
Wojciech Pakoca <59651907+wulf42@users.noreply.github.com>
2025-02-10 23:22:44 +0100

    Time: 214 ms (99.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    employee_id, department_id
+FROM 
+    Employee
+WHERE 
+    primary_flag = 'Y' OR 
+    employee_id NOT IN (SELECT employee_id FROM Employee WHERE primary_flag = 'Y'); 
