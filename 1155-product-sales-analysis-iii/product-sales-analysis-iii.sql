bf202e64c6abfc8a6513f07f43a9a88d108d3561
Wojciech Pakoca <59651907+wulf42@users.noreply.github.com>
2025-02-10 18:55:07 +0100

    Time: 1238 ms (90.60%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,19 +1,19 @@
 -- Write your PostgreSQL query statement below
-WITH min_year AS (SELECT 
-    product_id,
-    min(year) AS first_year
-FROM 
-    Sales
-GROUP BY 
-    product_id)
+WITH min_year AS (
+    SELECT 
+        product_id,
+        MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+)
 SELECT 
     S.product_id,
-    S.year as first_year,
+    M.first_year,
     S.quantity,
-    S.price 
-FROM
-    SALES as S
-join 
-    min_year as M
-on 
-    S.product_id = M.product_id AND M.first_year = S.year 
+    S.price
+FROM 
+    Sales AS S
+JOIN 
+    min_year AS M
+ON 
+    S.product_id = M.product_id AND S.year = M.first_year;
