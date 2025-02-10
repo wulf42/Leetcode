bad0b679291e27cd563588bb9980ccae638a825d
Wojciech Pakoca <59651907+wulf42@users.noreply.github.com>
2025-02-10 19:24:51 +0100

    Time: 565 ms (11.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -0,0 +1,14 @@
+-- Write your PostgreSQL query statement below
+SELECT COALESCE((
+    SELECT 
+        num 
+    FROM 
+        MyNumbers
+    GROUP BY 
+        num
+    HAVING 
+        COUNT(num) = 1
+    ORDER BY 
+        num DESC
+    LIMIT 1
+), NULL) as num ;
