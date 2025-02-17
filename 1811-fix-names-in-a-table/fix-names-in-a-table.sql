221452861231e6aff7344d58ae97c16b2b2aac57
Wojciech Pakoca <59651907+wulf42@users.noreply.github.com>
2025-02-17 22:19:51 +0100

    Time: 404 ms (96.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    user_id,
+    Concat(
+        Upper(substring(name,1,1)),
+        Lower(substring(name,2,length(name)))
+    ) as name  
+FROM
+    Users
+ORDER BY
+    user_id;
