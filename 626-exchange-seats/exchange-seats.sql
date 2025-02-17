137245aae5fb03f373870b04be544b758653c7d1
Wojciech Pakoca <59651907+wulf42@users.noreply.github.com>
2025-02-17 22:02:51 +0100

    Time: 315 ms (62.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    CASE
+    WHEN id % 2 = 0 THEN id - 1
+    WHEN id % 2 = 1 AND id NOT IN (SELECT MAX(id) FROM Seat) THEN id + 1
+    ELSE id
+    END AS id,
+    student
+FROM
+    Seat
+ORDER BY
+    id;
